fa624b6b6f827db20af81ef2326b95def0264f99
ellxor <ellxor@protonmail.ch>
2026-06-16 20:39:04 +0100

    Remove temporary compatibility header

--- pimcs/c_backend/compat.h
+++
@@ -1,8 +0,0 @@
-// Temporary compatibility fix for now to make code C99 compliant
-
-#include <stdbool.h>
-#include <stddef.h>
-
-#define auto __auto_type
-#define thread_local _Thread_local
-#define nullptr NULL
